ad2cabb50d54843ff7748ece85709c990b85c05e
nazar <nazar10fantom@gmail.com>
2025-06-07 22:54:42 +0200

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Provider } from '@/components/ui/provider';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Poppins } from 'next/font/google';
 import { ReactNode } from 'react';
 
@@ -10,48 +10,51 @@ const poppins = Poppins({
     display: 'swap'
 });
 
+export const viewport: Viewport = {
+    colorScheme: 'dark',
+    themeColor: '#2ECC71'
+};
+
 export const metadata: Metadata = {
-    title: 'HackCode'
-    // description:
-    //     'HackCode is a modern coding education platform to learn by building. Explore real-world challenges, interactive lessons, and guided paths.',
-    // keywords: [
-    //     'HackCode',
-    //     'coding',
-    //     'learn programming',
-    //     'interactive coding',
-    //     'students',
-    //     'school',
-    //     'college',
-    //     'education',
-    //     'programming',
-    //     'software development'
-    // ],
-    // authors: [{ name: 'Argon', url: 'https://www.linkedin.com/company/argonteam' }],
-    // creator: 'HackCode',
-    // // icons: {
-    // //     icon: '/favicon.ico',
-    // //     shortcut: '/favicon.ico',
-    // //     apple: '/apple-touch-icon.png'
-    // // },
-    // themeColor: '#2ECC71',
-    // colorScheme: 'dark',
-    // openGraph: {
-    //     title: 'HackCode – Learn to Code by Doing',
-    //     description: 'Master coding through hands-on projects and challenges. Built for learners and educators alike.',
-    //     url: 'https://hack-code.vercel.app',
-    //     siteName: 'HackCode',
-    //     images: [
-    //         {
-    //             url: '/og-image.png',
-    //             width: 1920,
-    //             height: 879,
-    //             alt: 'HackCode – Learn to Code by Doing'
-    //         }
-    //     ],
-    //     type: 'website',
-    //     locale: 'en_US'
-    // },
-    // metadataBase: new URL('https://hack-code.vercel.app')
+    title: 'HackCode',
+    description:
+        'HackCode is a modern coding education platform to learn by building. Explore real-world challenges, interactive lessons, and guided paths.',
+    keywords: [
+        'HackCode',
+        'coding',
+        'learn programming',
+        'interactive coding',
+        'students',
+        'school',
+        'college',
+        'education',
+        'programming',
+        'software development'
+    ],
+    authors: [{ name: 'Argon', url: 'https://www.linkedin.com/company/argonteam' }],
+    creator: 'HackCode',
+    icons: {
+        icon: '/favicon.ico',
+        shortcut: '/favicon.ico',
+        apple: '/apple-touch-icon.png'
+    },
+    openGraph: {
+        title: 'HackCode – Learn to Code by Doing',
+        description: 'Master coding through hands-on projects and challenges. Built for learners and educators alike.',
+        url: 'https://www.hack-code.net',
+        siteName: 'HackCode',
+        images: [
+            {
+                url: '/og-image.png',
+                width: 1920,
+                height: 879,
+                alt: 'HackCode – Learn to Code by Doing'
+            }
+        ],
+        type: 'website',
+        locale: 'en_US'
+    },
+    metadataBase: new URL('https://www.hack-code.net')
 };
 
 export default function RootLayout({ children }: { readonly children: ReactNode }) {
